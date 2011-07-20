29f512e715614a78fe60709348687ccf3a2d28fb
anthony taranto <anthony.taranto@rd.io>
2011-07-20 01:28:12 -0700

    dlopen() sparkle framework at app startup

--- Tepeyac.Mac/Main.cs
+++ Tepeyac.Mac/Main.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
 
 namespace Tepeyac.Mac
 {
@@ -6,6 +9,10 @@ namespace Tepeyac.Mac
 	{
 		static void Main (string[] args)
 		{
+			var contents = AppDomain.CurrentDomain.BaseDirectory.TrimEnd(Path.DirectorySeparatorChar);
+			var sparkle = Path.Combine(Directory.GetParent(contents).ToString(), "Frameworks", "Sparkle.framework", "Sparkle");
+			Dlfcn.dlopen(sparkle, 0);
+			
 			NSApplication.Init();
 			NSApplication.Main(args);
 		}
