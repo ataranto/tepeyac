3f85466e998f3f211513eefa261b4bd30170be27
anthony taranto <anthony.taranto@rd.io>
2011-08-14 16:18:50 -0700

    grab external sources with update.sh

---
+++ update.sh
@@ -0,0 +1,3 @@
+svn co http://retlang.googlecode.com/svn/trunk ext/retlang
+svn co https://htmlagilitypack.svn.codeplex.com/svn/trunk ext/htmlagilitypack
+hg clone https://hg01.codeplex.com/funq ext/funq
